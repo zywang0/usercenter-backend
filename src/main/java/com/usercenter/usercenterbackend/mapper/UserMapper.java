e9d60509b5411cfed1fd5ebbeb88bdf918511125
zywang0 <13679251160@163.com>
2023-03-11 23:03:45 -0800

    Init a spring boot application

---
+++ src/main/java/com/usercenter/usercenterbackend/mapper/UserMapper.java
@@ -0,0 +1,8 @@
+package com.usercenter.usercenterbackend.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.usercenter.usercenterbackend.model.User;
+
+public interface UserMapper extends BaseMapper<User> {
+
+}
