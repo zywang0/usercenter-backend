97a333a7d5e8f3e4dc5e65d12f4b14b8a3dab90a
zywang0 <13679251160@163.com>
2023-03-12 13:32:27 -0700

    Feat: Generate user model corresponding to db

--- src/main/java/com/usercenter/usercenterbackend/model/User.java
+++ src/main/java/com/usercenter/usercenterbackend/model/User.java
@@ -1,11 +1,81 @@
 package com.usercenter.usercenterbackend.model;
 
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import java.io.Serializable;
+import java.util.Date;
 import lombok.Data;
 
+/**
+ * 
+ * @TableName user
+ */
+@TableName(value ="user")
 @Data
-public class User {
+public class User implements Serializable {
+    /**
+     * 
+     */
+    @TableId(type = IdType.AUTO)
     private Long id;
-    private String name;
-    private Integer age;
+
+    /**
+     * 
+     */
+    private String username;
+
+    /**
+     * 
+     */
+    private String userAccount;
+
+    /**
+     * 
+     */
+    private String avatar;
+
+    /**
+     * 
+     */
+    private Integer gender;
+
+    /**
+     * 
+     */
+    private String userPassword;
+
+    /**
+     * 
+     */
+    private String phoneNum;
+
+    /**
+     * 
+     */
     private String email;
+
+    /**
+     * 
+     */
+    private Integer userStatus;
+
+    /**
+     * 
+     */
+    private Date createTime;
+
+    /**
+     * 
+     */
+    private Date updateTime;
+
+    /**
+     * 
+     */
+    private Integer isDelete;
+
+    @TableField(exist = false)
+    private static final long serialVersionUID = 1L;
 }
