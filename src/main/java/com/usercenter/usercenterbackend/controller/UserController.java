5c7eaca2a83a6adef93190cfccd1074cb3a94726
zywang0 <13679251160@163.com>
2023-05-05 15:41:34 -0700

    Feat: Implement search and delete users only if role is admin.

--- src/main/java/com/usercenter/usercenterbackend/controller/UserController.java
+++ src/main/java/com/usercenter/usercenterbackend/controller/UserController.java
@@ -1,17 +1,21 @@
 package com.usercenter.usercenterbackend.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.usercenter.usercenterbackend.model.User;
 import com.usercenter.usercenterbackend.model.request.UserLoginRequest;
 import com.usercenter.usercenterbackend.model.request.UserRegisterRequest;
 import com.usercenter.usercenterbackend.service.UserService;
 import org.apache.commons.lang3.StringUtils;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static com.usercenter.usercenterbackend.constant.UserConstant.ADMIN_ROLE;
+import static com.usercenter.usercenterbackend.constant.UserConstant.LOGIN_STATE;
 
 @RestController
 @RequestMapping("/user")
@@ -35,5 +39,30 @@ public class UserController {
         String userPassword = userLoginRequest.getUserPassword();
         if (StringUtils.isAnyBlank(userAccount, userPassword)) return null;
         return userService.userLogin(userAccount, userPassword, request);
+    }
+    @GetMapping("/search")
+    public List<User> searchUsers(String username, HttpServletRequest request) {
+        if (notAdmin(request)) return new ArrayList<>();
+        QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+        if (StringUtils.isNotBlank(username)) queryWrapper.like("username", username);
+        List<User> userList = userService.list(queryWrapper);
+        return userList.stream().map(user -> userService.getInsensitiveUser(user)).collect(Collectors.toList());
+    }
+
+    @PostMapping("/delete")
+    public boolean deleteUser(@RequestBody long id, HttpServletRequest request){
+        if (notAdmin(request)) return false;
+        if (id <= 0) return false;
+        return userService.removeById(id);
+    }
+
+    /**
+     * Only if role is Admin
+     * @return boolean
+     */
+    private static boolean notAdmin(HttpServletRequest request) {
+        Object userObj = request.getSession().getAttribute(LOGIN_STATE);
+        User user = (User) userObj;
+        return user == null || user.getRole() != ADMIN_ROLE;
     }
 }
