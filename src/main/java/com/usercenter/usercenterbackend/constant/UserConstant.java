c4fabe394554079f7096378f2d74e0dc3dc8201c
zywang0 <13679251160@163.com>
2023-05-05 15:35:18 -0700

    Feat: Add user role field for different permissions.

---
+++ src/main/java/com/usercenter/usercenterbackend/constant/UserConstant.java
@@ -0,0 +1,9 @@
+package com.usercenter.usercenterbackend.constant;
+
+public interface UserConstant {
+    String LOGIN_STATE = "loginState";
+
+    //--- role ---
+    int DEFAULT_ROLE = 0;
+    int ADMIN_ROLE = 1;
+}
--- src/main/java/com/usercenter/usercenterbackend/model/User.java
+++ src/main/java/com/usercenter/usercenterbackend/model/User.java
@@ -1,10 +1,10 @@
 package com.usercenter.usercenterbackend.model;
 
 import com.baomidou.mybatisplus.annotation.*;
+import lombok.Data;
 
 import java.io.Serializable;
 import java.util.Date;
-import lombok.Data;
 
 /**
  * 
@@ -19,28 +19,51 @@ public class User implements Serializable {
     @TableId(type = IdType.AUTO)
     private Long id;
 
+    /**
+     * 用户昵称
+     */
     private String username;
 
+    /**
+     * 用户账号
+     */
     private String userAccount;
 
+    /**
+     * 头像URL
+     */
     private String avatar;
 
     private Integer gender;
 
+    /**
+     * 用户密码
+     */
     private String userPassword;
 
     private String phoneNum;
 
     private String email;
 
+    /**
+     * 0 - 正常
+     */
     private Integer userStatus;
 
     private Date createTime;
 
     private Date updateTime;
 
+    /**
+     * 是否删除
+     */
     @TableLogic
     private Integer isDelete;
+
+    /**
+     * 用户角色：User - 0, Admin - 1
+     */
+    private Integer role;
 
     @TableField(exist = false)
     private static final long serialVersionUID = 1L;
